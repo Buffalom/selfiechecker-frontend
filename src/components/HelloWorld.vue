a088485cc2dbb6175fef48abfb63625974bcc42c
Florence Meier <florence.meier@swisscom.com>
2018-10-26 21:48:36 +0200

    added img in main page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,8 +12,24 @@
       <v-icon>camera_alt</v-icon>
     </v-btn>
 
-    
-        
+    <v-layout row wrap>
+      <v-flex xs12 md10>
+        <v-card class="my-1">
+          <v-layout align-center justify-center>
+              <v-img :src="source"></v-img>
+          </v-layout>
+        </v-card>
+        <v-card class="my-1">
+          <v-layout align-center justify-center>
+              <v-img :src="outcome"></v-img>
+          </v-layout>
+          <v-card-actions>
+            <v-btn color="primary">Add to fanbook</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      </v-layout>
+
   </div>
 </v-container>
 </template>
