0516e9cdcc2d2590babb7fe6f4363b0d227180c0
DESKTOP-K22DR98\Super Mario <visomario@hotmail.com>
2018-10-26 18:54:09 +0200

    Upload picture

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,21 @@
 <template>
+<v-container grid-list-md text-xs-center>
   <div class="hello">
     <v-btn color='success'> FME </v-btn>
+    <input id="fileUpload" type="file" accept="image/*;capture=camera" hidden>
     <v-btn
-              color="pink"
-              dark
-              small    
-              fab
-            >
-              <v-icon>add</v-icon>
-            </v-btn>
+      @click="chooseFiles()"
+      color="pink"
+      dark    
+      fab
+      >
+      <v-icon>camera_alt</v-icon>
+    </v-btn>
+
+    
+        
   </div>
+</v-container>
 </template>
 
 <script>
@@ -17,7 +23,18 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  methods: {
+    chooseFiles() {
+      document.getElementById("fileUpload").click();
+    },
+    onUpload() {
+      // upload file
+    }
   }
 };
 </script>
+
+
+
 
