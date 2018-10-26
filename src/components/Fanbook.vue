be4534d0232447840c6f84a6a5d481b096f5a155
Florence Meier <florence.meier@swisscom.com>
2018-10-26 17:14:39 +0200

    Added fanbook page and linked it

---
+++ src/components/Fanbook.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <!-- HERE COMES ICONS -->
+      <!-- Title -->
+      <h4>Fanbook of {{user.name}}</h4>
+      <!-- CARDS -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+      return {
+        user: {
+            name: "SuperFan",
+            wins: 2
+        }
+      }
+  }
+};
+</script>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <v-btn color='success'> FME </v-btn>
+    <v-btn color='success' @click="openFanbook">FANBOOK</v-btn>
     <v-btn
               color="pink"
               dark
@@ -14,9 +14,11 @@
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  name: 'HelloWorld',
+  methods: {
+    openFanbook() {
+      this.$router.push('/fanbook');
+    }
   }
 };
 </script>
