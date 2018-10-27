51bf6a0830f5f7eafd9a75185a58095b76f1979f
Diggelmann Tom, GHR-SRT-NEX-C-1 <Tom.Diggelmann@swisscom.com>
2018-10-27 09:40:07 +0200

    Deploying

---
+++ precache-manifest.255a338809aaff3a87540673627573b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/selfiechecker-frontend/robots.txt"
+  },
+  {
+    "revision": "d5ea92ed0fcc811328e4",
+    "url": "/selfiechecker-frontend/js/chunk-vendors.41cf6c55.js"
+  },
+  {
+    "revision": "5a3a72c956fad19e3b94",
+    "url": "/selfiechecker-frontend/js/app.0272962f.js"
+  },
+  {
+    "revision": "662a714c179b85110fbe1c2e4cc83b7c",
+    "url": "/selfiechecker-frontend/index.html"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/selfiechecker-frontend/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/selfiechecker-frontend/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/selfiechecker-frontend/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/selfiechecker-frontend/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "d5ea92ed0fcc811328e4",
+    "url": "/selfiechecker-frontend/css/chunk-vendors.5d649c7a.css"
+  },
+  {
+    "revision": "5a3a72c956fad19e3b94",
+    "url": "/selfiechecker-frontend/css/app.0fc4d871.css"
+  }
+];
